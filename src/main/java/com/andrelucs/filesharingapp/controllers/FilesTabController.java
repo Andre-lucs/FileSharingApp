b4ab94ef5514f1d34d2a3a8cc232adf0c62fd315
Andre-lucs <andreltd312@gmail.com>
2025-01-09 18:36:48 -0300

    feat: File sorting in FilesTabController

--- src/main/java/com/andrelucs/filesharingapp/controllers/FilesTabController.java
+++ src/main/java/com/andrelucs/filesharingapp/controllers/FilesTabController.java
@@ -5,6 +5,7 @@ import com.andrelucs.filesharingapp.components.FileItem;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.SplitPane;
 import javafx.scene.image.Image;
@@ -13,12 +14,15 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.TilePane;
 
 import java.io.File;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Objects;
 
 public class FilesTabController {
     @FXML
     Label folderName;
+    @FXML
+    ComboBox<String> sortComboBox;
     @FXML
     TilePane filesDisplayContainer;
     @FXML
@@ -36,6 +40,7 @@ public class FilesTabController {
 
     @FXML
     public void initialize() {
+        sortComboBox.setOnAction(event -> updateSharedFilesDisplay());
         uploadingOverlay.setVisible(false);
         splitPane.setDividerPosition(0, 1);
         // Scale the image display
@@ -76,9 +81,24 @@ public class FilesTabController {
         uploadingOverlay.setVisible(FileSharingApplication.isBeingUploaded(file));
     }
 
-    public void updateSharedFilesDisplay() { // TODO Otimizar
+    public void updateSharedFilesDisplay() {
+        String selectedSort = sortComboBox.getSelectionModel().getSelectedItem();
+        if (selectedSort == null) {
+            selectedSort = "Name";
+        }
+        if (selectedSort.equals("Size"))
+            updateSharedFilesDisplay(Comparator.comparing(File::length));
+        else if (selectedSort.equals("Date"))
+            updateSharedFilesDisplay(Comparator.comparing(File::lastModified).reversed());
+        else
+            updateSharedFilesDisplay(Comparator.comparing(File::getName));
+    }
+
+    public void updateSharedFilesDisplay(Comparator<File> comparator) { // TODO Otimizar
         List<File> sharedFiles = FileSharingApplication.getSharedFiles();
         List<Node> children = filesDisplayContainer.getChildren();
+
+        sharedFiles.sort(comparator);
 
         children.clear();
         sharedFiles.forEach(file -> {
